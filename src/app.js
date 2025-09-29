d7a3c70c324d0073afcf523766fb5f3b7d02033e
Paulo Roberto <devpaulorag@gmail.com>
2025-09-29 20:20:55 -0300

    feat: adiciona endpoint GET /users para lsitar usuários

--- src/app.js
+++ src/app.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const app = express();
+const usuarios = [
+    { id: 1, nome: "Paulo Roberto" },
+    { id: 2, nome: "Ana Carolina" },
+    { id: 3, nome: "Bruno Costa" },
+];
 
 app.get("/", (req, res) => {
     res.json({ message: "API de Treinmaneto está no ar!" });
@@ -7,6 +12,10 @@ app.get("/", (req, res) => {
 
 app.get("/status", (req, res) => {
     res.status(200).json({ status: "ok" });
+});
+
+app.get("/users", (req, res) => {
+    res.status(200).json(usuarios);
 });
 
 module.exports = app;
